5e600e869b38af3f85fcfa2004ba8352e4dae887
Raj2340 <164938911+Raj2340@users.noreply.github.com>
2025-04-22 01:53:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+// Variable Declaration
+
+    const loginBtn = document.querySelector("#login");
+    const registerBtn = document.querySelector("#register");
+    const loginForm = document.querySelector(".login-form");
+    const registerForm = document.querySelector(".register-form");
+
+// Login button function
+loginBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundcolor = "#21264d";
+    registerBtn.style.backgroundcolor = "rgba(255, 255, 255, 0.2)";
+
+    loginForm.style.left = "50%";
+    registerForm.style.left ="-50%";
+
+    loginForm.style.opacity = 1;
+    registerForm.style.opacity = 0;
+
+    document.querySelector(".col-1").style.borderRadius = "0 30% 20% 0";
+})
+
+
+// Register button function
+registerBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundcolor = "rgba(255, 255, 255, 0.2);";
+    registerBtn.style.backgroundcolor = "#21264d";
+
+    loginForm.style.left = "150%";
+    registerForm.style.left ="50%";
+
+    loginForm.style.opacity = 0;
+    registerForm.style.opacity = 1;
+
+    document.querySelector(".col-1").style.borderRadius = "0 20% 30% 0";
+})
+
